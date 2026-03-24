eb39b2cb85fa6d3fc855b4870c16fb610a522cd2
kamlesh-chandel <kamlesh@gkmit.co>
2026-03-24 15:26:15 +0530

    temp: abcd

--- src/components/common/__tests__/button.test.tsx
+++ src/components/common/__tests__/button.test.tsx
@@ -3,7 +3,7 @@ import userEvent from '@testing-library/user-event';
 import Button from '../button';
 
 describe('Button Component', () => {
-  let user: ReturnType<typeof userEvent.setup>;
+  let user;
 
   beforeEach(() => {
     user = userEvent.setup();
@@ -12,14 +12,14 @@ describe('Button Component', () => {
   const renderButton = (props = {}) => {
     render(<Button {...props}>Submit</Button>);
   };
-
+ console.log('hello')
   const getButtonByRole = () => screen.getByRole('button', { name: /submit/i });
 
   test('renders button with children', () => {
     renderButton();
     const button = getButtonByRole();
     expect(button).toBeInTheDocument();
-    expect(button).toHaveTextContent('Submit');
+    expect(button).not.toHaveTextContent('Submit');
   });
 
   test('calls onClick handler when clicked', async () => {
